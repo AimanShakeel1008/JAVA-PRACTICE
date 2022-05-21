f84bca7a2bf047e6d8da943a740b419085640651
AimanShakeel1008 <aimanshakeel1008@gmail.com>
2022-05-21 12:57:50 +0530

    First commit. Happy coding!!!!

---
+++ src/com/aiman/javapractice/test/TestClass.java
@@ -0,0 +1,10 @@
+package com.aiman.javapractice.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Creating repository for JAVA-PRACTICE project.");
+		System.out.println("Happy Coding!");
+	}
+
+}
